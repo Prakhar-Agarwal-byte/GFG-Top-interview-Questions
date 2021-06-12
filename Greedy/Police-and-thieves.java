671d9d56921858b5fddbaa9758f7113b4a067360
Prakhar Agarwal <prakhar@everything.net.in>
2021-06-12 11:49:20 +0530

    Create Police-and-thieves.java

---
+++ Greedy/Police-and-thieves.java
@@ -0,0 +1,23 @@
+class Solution {
+    static int catchThieves(char arr[], int n, int k) {
+        Queue<Integer> thieves = new LinkedList<>();
+        Queue<Integer> police = new LinkedList<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 'P') police.add(i);
+            else thieves.add(i);
+        }
+        while (!thieves.isEmpty() && !police.isEmpty()) {
+            if (Math.abs(thieves.peek() - police.peek()) <= k) {
+                count++;
+                thieves.poll();
+                police.poll();
+            } else if (thieves.peek() < police.peek()) {
+                thieves.poll();
+            } else {
+                police.poll();
+            }
+        }
+        return count;
+    }
+}
