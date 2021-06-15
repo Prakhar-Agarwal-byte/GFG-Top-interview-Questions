3b9d3e5f09a016a33fd6970e9c03c92aee9aa3dc
Prakhar Agarwal <prakhar@everything.net.in>
2021-06-15 15:27:32 +0530

    Update Kth-largest-element-in-stream.java

--- Heap/Kth-largest-element-in-stream.java
+++ Heap/Kth-largest-element-in-stream.java
@@ -3,13 +3,9 @@ class Solution {
         int[] ans = new int[n];
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
-            if (pq.size() < k) {
-                pq.add(arr[i]);
-            } else {
-                if (arr[i] > pq.peek()) {
-                    pq.poll();
-                    pq.add(arr[i]);
-                }
+            pq.add(arr[i]);
+            if (pq.size() > k) {
+                pq.remove();
             }
             if (pq.size() < k) {
                 ans[i] = -1;
