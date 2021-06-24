d9e2076604777d10290052ec903ddda44188fc35
Prakhar Agarwal <prakhar@everything.net.in>
2021-06-24 16:52:24 +0530

    Create Recover BST.java

---
+++ BST/Recover BST.java
@@ -0,0 +1,31 @@
+// This approach uses O(n) space. but optimized approach uses morris traversal which I have'nt studied yet. Will update it once I learn it.
+
+class Solution {
+    private TreeNode first;
+    private TreeNode second;
+    private TreeNode pre;
+    public void recoverTree(TreeNode root) {
+        if(root==null) return;
+        first = null;
+        second = null;
+        pre = null;
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+    
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        
+        if(first==null && (pre==null ||pre.val>=root.val)){
+            first = pre;
+        }
+        if(first!=null && pre.val>=root.val){
+            second = root;
+        }
+        pre = root;
+        inorder(root.right);
+    }
+}
