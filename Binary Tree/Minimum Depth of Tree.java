e12c896af802522c5600dc9f1e88fc7453e06937
Prakhar Agarwal <prakhar@everything.net.in>
2021-07-16 23:08:26 +0530

    Update Minimum Depth of Tree.java

--- Binary Tree/Minimum Depth of Tree.java
+++ Binary Tree/Minimum Depth of Tree.java
@@ -1,9 +1,6 @@
-public class Solution {
-    public int minDepth(TreeNode root) {
-        if(root == null) return 0;
-        int left = minDepth(root.left);
-        int right = minDepth(root.right);
-        return (left == 0 || right == 0) ? left + right + 1: Math.min(left,right) + 1;
-       
-    }
+public static int minDepth(TreeNode root) {
+	if (root == null)	return 0;
+	if (root.left == null)	return minDepth(root.right) + 1;
+	if (root.right == null) return minDepth(root.left) + 1;
+	return Math.min(minDepth(root.left),minDepth(root.right)) + 1;
 }
