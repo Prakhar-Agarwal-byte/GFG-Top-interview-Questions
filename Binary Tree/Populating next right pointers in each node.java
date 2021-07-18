32008f0972abfc06063add7b43791ec509a477a6
Prakhar Agarwal <prakhar@everything.net.in>
2021-07-18 18:30:57 +0530

    Update Populating next right pointers in each node.java

--- Binary Tree/Populating next right pointers in each node.java
+++ Binary Tree/Populating next right pointers in each node.java
@@ -1,3 +1,5 @@
+// Recursive
+
 class Solution {
     public Node connect(Node root) {
         dfs(root, null);        
@@ -9,5 +11,23 @@ class Solution {
         curr.next = next;
         dfs(curr.left, curr.right);
         dfs(curr.right, curr.next == null ? null : curr.next.left);
+    }
+}
+
+// Iterative
+
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode level_start=root;
+        while(level_start!=null){
+            TreeLinkNode cur=level_start;
+            while(cur!=null){
+                if(cur.left!=null) cur.left.next=cur.right;
+                if(cur.right!=null && cur.next!=null) cur.right.next=cur.next.left;
+                
+                cur=cur.next;
+            }
+            level_start=level_start.left;
+        }
     }
 }
