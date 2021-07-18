9f6975ac5227110cbd70ed2c3f79cde0efe5e79e
Prakhar Agarwal <prakhar@everything.net.in>
2021-07-18 14:33:21 +0530

    Create Preorder Iterative.java

---
+++ Binary Tree/Preorder Iterative.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+
+  public List<Integer> preorderTraversal(TreeNode root) {
+    Stack<TreeNode> st = new Stack<>();
+    List<Integer> res = new ArrayList<>();
+    st.push(root);
+    while (!st.isEmpty()) {
+      TreeNode tempNode = st.pop();
+      res.add(tempNode.val);
+      if (tempNode.right != null) {
+        st.push(tempNode.right);
+      }
+      if (tempNode.left != null) {
+        st.push(tempNode.left);
+      }
+    }
+    return res;
+  }
+}
