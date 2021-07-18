390a0fc4570c7875882b334961d483765d10fd38
Prakhar Agarwal <prakhar@everything.net.in>
2021-07-18 14:34:31 +0530

    Update Preorder Iterative.java

--- Binary Tree/Preorder Iterative.java
+++ Binary Tree/Preorder Iterative.java
@@ -7,6 +7,7 @@ class Solution {
   public List<Integer> preorderTraversal(TreeNode root) {
     Stack<TreeNode> st = new Stack<>();
     List<Integer> res = new ArrayList<>();
+    if (root == null) return res;
     st.push(root);
     while (!st.isEmpty()) {
       TreeNode tempNode = st.pop();
