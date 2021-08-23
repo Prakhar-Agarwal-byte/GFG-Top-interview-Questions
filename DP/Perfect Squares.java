1d254dfdb570908c104f56a35bf2cc649fdde7fa
Prakhar Agarwal <prakhar@everything.net.in>
2021-08-23 16:52:24 +0530

    Create Perfect Squares.java

---
+++ DP/Perfect Squares.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int numSquares(int n) {
+    int[] dp = new int[n + 1];
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++) {
+      int j = 1;
+      int min = Integer.MAX_VALUE;
+      while (i - j * j >= 0) {
+        min = Math.min(min, dp[i - j * j] + 1);
+        j++;
+      }
+      dp[i] = min;
+    }
+    return dp[n];
+  }
+}
