e03b7f393d08511b3bd67d4c7087fd27a6fd55bf
Prakhar Agarwal <prakhar@everything.net.in>
2021-08-27 19:28:29 +0530

    Update Edit Distance.java

--- DP/Edit Distance.java
+++ DP/Edit Distance.java
@@ -1,28 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
-  int[][] dp;
-
   public int minDistance(String word1, String word2) {
-    dp = new int[word1.length() + 1][word2.length() + 1];
-    for (int[] rows : dp) {
-      Arrays.fill(rows, Integer.MAX_VALUE);
+    int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+    for (int i = 0; i <= word1.length(); i++) {
+      dp[i][0] = i;
     }
-    return util(word1, word2, word1.length(), word2.length());
-  }
-
-  private int util(String word1, String word2, int m, int n) {
-    if (m == 0)
-      return n;
-    if (n == 0)
-      return m;
-    if (dp[m][n] != Integer.MAX_VALUE) {
-      return dp[m][n];
+    for (int i = 0; i <= word2.length(); i++) {
+      dp[0][i] = i;
     }
-    if (word1.charAt(m - 1) == word2.charAt(n - 1)) {
-      return dp[m][n] = util(word1, word2, m - 1, n - 1);
+    for (int i = 1; i <= word1.length(); i++) {
+      for (int j = 1; j <= word2.length(); j++) {
+        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+        }
+      }
     }
-    return dp[m][n] = 1 + Math.min(util(word1, word2, m, n - 1),
-        Math.min(util(word1, word2, m - 1, n), util(word1, word2, m - 1, n - 1)));
+    return dp[word1.length()][word2.length()];
   }
 }
