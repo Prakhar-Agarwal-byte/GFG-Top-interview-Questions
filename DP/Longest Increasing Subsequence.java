8a8db940819f2e055c672f8373f0f0c47b75e436
Prakhar Agarwal <prakhar@everything.net.in>
2021-08-28 10:57:36 +0530

    Update Longest Increasing Subsequence.java

--- DP/Longest Increasing Subsequence.java
+++ DP/Longest Increasing Subsequence.java
@@ -2,13 +2,12 @@ import java.util.Arrays;
 
 class Solution {
   public int lengthOfLIS(int[] nums) {
-    int[] dp = new int[nums.length + 1];
+    int[] dp = new int[nums.length];
     Arrays.fill(dp, 1);
-    dp[0] = 0;
     int max = 1;
-    for (int i = 2; i <= nums.length; i++) {
-      for (int j = 1; j < i; j++) {
-        if (nums[j - 1] < nums[i - 1]) {
+    for (int i = 1; i < nums.length; i++) {
+      for (int j = 0; j < i; j++) {
+        if (nums[j] < nums[i ]) {
           dp[i] = Math.max(dp[i], 1 + dp[j]);
         }
       }
