55672a05498e18f742a15a0b9f2d52ff24fff378
Prakhar Agarwal <prakhar@everything.net.in>
2021-09-09 15:34:59 +0530

    Update N-meetings-in-one-room.java

--- Greedy/N-meetings-in-one-room.java
+++ Greedy/N-meetings-in-one-room.java
@@ -1,34 +1,28 @@
 class Solution {
-    public static int maxMeetings(int start[], int end[], int n) {
-        Meeting[] meetings = new Meeting[n];
-        for (int i = 0; i < n; i++) {
-            meetings[i] = new Meeting(start[i], end[i]);
-        }
-        Arrays.sort(meetings, new Comparator<Meeting>() {
-            @Override
-            public int compare(Meeting o1, Meeting o2) {
-                if (o1.end > o2.end) return 1;
-                return 0;
-            }
-        });
-        
-        int index = 0;
-        int count = 1;
-        for (int i = 1; i < n; i++) {
-            if (meetings[i].start > meetings[index].end) {
-                index = i;
-                count++;
-            }
-        }
-        return count;
+  public static int maxMeetings(int start[], int end[], int n) {
+    Meeting[] arr = new Meeting[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = new Meeting(start[i], end[i]);
     }
+    Arrays.sort(arr, (a, b) -> (a.end - b.end));
+    int count = 1;
+    int endC = arr[0].end;
+    for (int i = 1; i < n; i++) {
+      if (arr[i].start > endC) {
+        count++;
+        endC = arr[i].end;
+      }
+    }
+    return count;
+  }
+}
 
-    static class Meeting {
-        int start;
-        int end;
-        Meeting(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
+class Meeting {
+  int start;
+  int end;
+
+  Meeting(int start, int end) {
+    this.start = start;
+    this.end = end;
+  }
 }
